324f2c56b2810e5e500368f8108cefd63b807c49
nawt <jtsiems@me.com>
2014-11-26 00:37:25 +0100

    Prototyp
    Skript für Movement angefangen,
    leider bewegt sich der Rigidbody beim Beschleunigen nur auf einer Achse,
    nicht in die Richtung in die er zeigt :/

---
+++ WaterMark/Assets/move_improv.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move_improv : MonoBehaviour {
+
+	public float vor = 1f;
+	public float zur = 1f;
+	public float rota = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	void Update () 
+	{
+		// Movement mit Trägheit, momentan bewegt sich der Rigidbody aber auf einer Achse
+		// unabhängig von der Ausrichtung :/
+		if(Input.GetKey(KeyCode.UpArrow)){
+			rigidbody.AddForce(Vector3.forward * vor, ForceMode.Force);
+			if(Input.GetKey(KeyCode.LeftArrow))
+				rigidbody.AddTorque(Vector3.up * -rota, ForceMode.Force);
+			if(Input.GetKey(KeyCode.RightArrow))
+				rigidbody.AddTorque (Vector3.up * rota, ForceMode.Force);}
+
+		if(Input.GetKey(KeyCode.DownArrow)){
+			rigidbody.AddForce(-Vector3.forward * (vor/2), ForceMode.Force);
+		if(Input.GetKey(KeyCode.LeftArrow))
+				rigidbody.AddTorque(Vector3.up * -rota, ForceMode.Force);
+		if(Input.GetKey(KeyCode.RightArrow))
+				rigidbody.AddTorque (Vector3.up * rota, ForceMode.Force);}
+}
+
+}
