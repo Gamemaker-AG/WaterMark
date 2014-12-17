b647eceb86e6243b1d0f2f4f5f8a494fc5549f6e
ThePseud0o <maps.arne@googlemail.com>
2014-12-17 19:19:29 +0100

    Kugeln autodestroy

---
+++ WaterMark/Assets/AutoDestruct.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestruct : MonoBehaviour
+{
+
+	public float time = 10;
+	// Use this for initialization
+	void Start ()
+	{
+		Destroy (gameObject, time);
+	}
+}
