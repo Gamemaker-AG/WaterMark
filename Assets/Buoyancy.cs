6472adb673012be8b0f1f0d3df69756ed5f2f880
Kevin Wessolleck <2wessoll@informatik.uni-hambur.de>
2015-01-29 22:31:09 +0100

    "Buoyancy"
    minor changes and edit a comment

--- Assets/Buoyancy.cs
+++ Assets/Buoyancy.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class Buoyancy : MonoBehaviour {
-	public float UpwardForce = 12.72f; // 9.81 is the opposite of the default gravity, which is 9.81. If we want the boat not to behave like a submarine the upward force has to be higher than the gravity in order to push the boat to the surface
+	public float vertForce = 12.72f;  // 9.81 is the opposite of the default gravity, which is -9.81
+        //If we want the boat not to behave like a submarine the upward force has to be higher than the gravity in order to push the boat to the surface
+        //to prevent continuous bobbing the vertical force needs to be calculated by the depth the object is dipping the water
 	private bool isInWater = false;
 	
 	void OnTriggerEnter(Collider collidier) {
@@ -18,9 +20,9 @@ public class Buoyancy : MonoBehaviour {
 	void FixedUpdate() {
 		if(isInWater) {
 			// apply upward force
-			Vector3 force = transform.up * UpwardForce;
+			Vector3 force = transform.up * vertForce;
 			this.rigidbody.AddRelativeForce(force, ForceMode.Acceleration);
-			Debug.Log("Upward force: " + force+" @"+Time.time);
+			//Debug.Log("vertical force: " + force+" @"+Time.time);
 		}
 	}
 }
