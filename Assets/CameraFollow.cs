26707787834fb54d5bfcc255e591164bd17c4af1
nawt <jtsiems@me.com>
2015-01-29 20:35:27 +0100

    Buoyancy + Camera Follow Script
    the script isn't working properly right now, still needs rotation etc...

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	private float camx = 0f;
+	private float camy = 0f;
+	private float camz = 0f;
+
+	public float rotationx = 0f;
+	public float rotationy = 0f;
+	public float rotationz = 0f;
+	
+
+	// Use this for initialization
+	void Start () {
+		var pos = transform.position;
+		camx = pos.x;
+		camy = pos.y;
+		camz = pos.z;
+
+		//transform.position = new Vector3(camx, camy, camz);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(camx + (GameObject.Find ("RED").transform.position.x), camy, camz + (GameObject.Find ("RED").transform.position.z));
+		//transform.rotation.x = rotationx;
+
+
+	
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour 
+{
+
+	public float acc = 1f;
+	public float torque = 1f;
+	public KeyCode Up;
+	public KeyCode Down;
+	public KeyCode Left;
+	public KeyCode Right;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	void Update () 
+	{
+		// Movement mit Trägheit
+		if(Input.GetKey(Up))
+		{
+			rigidbody.AddForce(transform.forward * acc * Time.deltaTime, ForceMode.Force);
+			if(Input.GetKey(Left))
+				rigidbody.AddTorque(transform.up * -torque * Time.deltaTime, ForceMode.Force);
+			if(Input.GetKey(Right))
+				rigidbody.AddTorque (transform.up * torque * Time.deltaTime, ForceMode.Force);
+		}
+
+		if(Input.GetKey(Down))
+		{
+			rigidbody.AddForce(-transform.forward * (acc/2) * Time.deltaTime, ForceMode.Force);
+			if(Input.GetKey(Left))
+				rigidbody.AddTorque(transform.up * torque * Time.deltaTime, ForceMode.Force);
+			if(Input.GetKey(Right))
+				rigidbody.AddTorque (transform.up * -torque * Time.deltaTime, ForceMode.Force);
+		}
+
+		if(Input.GetKey(Left))
+			rigidbody.AddTorque(transform.up * (-torque/2) * Time.deltaTime, ForceMode.Force);
+		if(Input.GetKey (Right))
+			rigidbody.AddTorque (transform.up * (torque/2) * Time.deltaTime, ForceMode.Force);
+	}
+
+}
