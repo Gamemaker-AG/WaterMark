4bd7dcc562d2009d2be5be32febc82db0e11a87f
Kevin Wessolleck <2wessoll@informatik.uni-hambur.de>
2015-01-30 23:30:23 +0100

    Rays/Raycasts
    not used yet,
    trying to use them to get the height of the waterLevel

--- Assets/Floater.cs
+++ Assets/Floater.cs
@@ -5,15 +5,18 @@ public class Floater : MonoBehaviour {
 	public float waterLevel, floatHeight;
 	public Vector3 buoyancyCentreOffset;
 	public float bounceDamp;
-	
-	
+   // public Ray rayDown; // = new Ray(transform.position, Vector3.down);
+
 
 	void FixedUpdate () {
+        //waterLevel calculation
+        //waterLevel = new RaycastHit().point.y; //needs some research on usage of Raycasts
+
 		Vector3 actionPoint = transform.position + transform.TransformDirection(buoyancyCentreOffset);
 		float forceFactor = 1f - ((actionPoint.y - waterLevel) / floatHeight);
 		
 		if (forceFactor > 0f) {
-			Vector3 uplift = -Physics.gravity * (forceFactor - rigidbody.velocity.y * bounceDamp);
+			Vector3 uplift = -Physics.gravity * (forceFactor - rigidbody.velocity.y * (1 / bounceDamp));
 			rigidbody.AddForceAtPosition(uplift, actionPoint);
 		}
 	}
