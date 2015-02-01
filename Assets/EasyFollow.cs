eb1a3ce18f91a21301a00f1d19b38c4bd3dc626a
nawt <jtsiems@me.com>
2015-02-01 15:56:36 +0100

    EasyFollow
    the old script didn't work the right way, so I replaced it with an easy
    one that just cancels the y-movement of the parent(the ships in this
    case)

---
+++ Assets/EasyFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasyFollow : MonoBehaviour {
+
+	public string ship = null;
+	private float heightboat = 0f;
+	private float heightcam = 0f;
+	private float differ = 0f;
+
+	// Use this for initialization
+	void Start () {
+		heightboat = GameObject.Find (ship).transform.position.y;
+		heightcam = this.transform.position.y;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		differ = heightboat - GameObject.Find (ship).transform.position.y;
+		heightboat = GameObject.Find (ship).transform.position.y;
+
+		transform.position = new Vector3(transform.position.x,heightcam + differ, transform.position.z);
+	
+	}
+}
