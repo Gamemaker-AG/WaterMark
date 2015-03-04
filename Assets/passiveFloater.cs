e14fbda882850a94ef97645ac22d9f2735f82e2b
Kevin Wessolleck <2wessoll@informatik.uni-hambur.de>
2015-03-05 00:28:16 +0100

    Passive floating basic functionality
    adds a lifting force based on the volume of the floated object and the
    percentage of how far the object is in the water.
    somehow the volume represents the mass of the object though. That means
    volumes smaller than 1 will sink.

--- Assets/passiveFloater.cs
+++ Assets/passiveFloater.cs
@@ -6,49 +6,79 @@ using System.Collections.Generic;
 public class passiveFloater : MonoBehaviour
 {
     //public List<GameObject> _items; 
-    public Dictionary<GameObject, Vector3> _items;
+    //public Dictionary<Collider, Vector3> _items;
+    public Dictionary<Collider, float> _originalDrag;
+    public Dictionary<Collider, float> _volumeInformation;
+    public List<GameObject> listOfTrackedObjects;
+    public float _factor;
+    public float _test;
+    public Vector3 _force;
 
-
-
-    // Use this for initialization
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //layermask = test.value;
+        //_items = new Dictionary<Collider, Vector3>();
+        _originalDrag = new Dictionary<Collider, float>();
+        _volumeInformation = new Dictionary<Collider, float>();
+        listOfTrackedObjects = new List<GameObject>();
     }
 
     //called with FixedUpdate timer for every Collider other that is touching the trigger.
     void OnTriggerStay(Collider other)
     {
+
         if (other.rigidbody != null)
         {
-            float test = (other.transform.position - transform.position).magnitude;
+            //GenericFloating(other, test);
             float factor;
-            if (test > -other.bounds.extents.magnitude)
+            float test = other.transform.position.y - transform.position.y;
+
+            if (test > -other.bounds.extents.y)
             {
-                factor = 0.5f; // TODO - how far the bounds of other intersect the water?
+                factor = 1 /
+                    (other.bounds.size.y / (other.bounds.extents.y - test));
+                if (factor == 0) { factor = Mathf.Epsilon; }
+
+                _factor = factor;
+                //Debug.Log("size.y = " + other.bounds.size.y);
             }
             else
             {   //if the object is completly inside the water
                 factor = 1;
+
+                _factor = factor;
             }
-            _items[other.gameObject] = -Physics.gravity * other.rigidbody.mass * factor; // TODO - 
-            //instead of the mass of other the mass of the water in space of the bound of other.
+
+            Vector3 force;
+            force = (-Physics.gravity * _volumeInformation[other] * factor); //TODO - foo
+
+            other.rigidbody.AddForce(force, ForceMode.Acceleration);
+
+            _factor = factor;
+            _force = force;
+            _test = test;
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        _items.Add(other.gameObject, Vector3.zero);
+        //_items.Add(other, Vector3.zero);
+        _originalDrag.Add(other, other.rigidbody.drag);
+        other.rigidbody.drag *= 3;
+        listOfTrackedObjects.Add(other.gameObject);
+
+        //Volume
+        float mass = other.rigidbody.mass;
+        other.rigidbody.SetDensity(1);
+        _volumeInformation.Add(other, other.rigidbody.mass); //the mass of an object with a density of 1 represents it's volume
+        other.rigidbody.mass = mass;
+        Debug.Log("Volume of " + other.gameObject + ": " + _volumeInformation[other]);
     }
     void OnTriggerExit(Collider other)
     {
-        _items.Remove(other.gameObject);
+        //_items.Remove(other);
+        other.rigidbody.drag = _originalDrag[other];
+        _originalDrag.Remove(other);
+        listOfTrackedObjects.Remove(other.gameObject);
     }
 
 }
